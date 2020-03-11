3a72438c668cf3178e078659174628708c541a62
chn2987 <646572031@qq.com>
2020-03-11 22:57:20 +0800

    删减test_02.py内容

--- test_02.py
+++ test_02.py
@@ -7,16 +7,6 @@ print(type(a))#打印数据类型
 if isinstance(a,str):
     print('变量a的类型为str')
     if a.isalpha():
-        print('字符串全部是字母')
-    elif a.isspace():#如空格
-        print('字符串为空')
-    elif a.isdigit():
-	    print('字符串全部是数字')
-elif isinstance(a,int):
-    print('变量a的类型为int')
-<<<<<<< HEAD
-    print('变量a的22222t')
-=======
->>>>>>> origin/chen_test
+        pass
 
 
